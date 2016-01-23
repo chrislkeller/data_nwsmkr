507ae9e5494c139f78aeed207c03e45489bb6f43
Chris Keller <christopherlawrencekeller@gmail.com>
2016-01-22 17:44:10 -0800

    updates urls.py to add API resources

--- gauging_el_nino/urls.py
+++ gauging_el_nino/urls.py
@@ -13,9 +13,38 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+import logging
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from django.contrib import admin
+from tastypie.api import Api
+from gauging_el_nino.api import RainGaugeResource, RainGaugeReadingResource
+
+logger = logging.getLogger("gauging_rainfall")
+
+# invoke the api
+v1_api = Api(api_name='v1')
+v1_api.register(RainGaugeResource())
+v1_api.register(RainGaugeReadingResource())
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r"^admin/", include(admin.site.urls)),
+
+    # tastypie api
+    url(r'^api/', include(v1_api.urls)),
+
+    # batch edit in admin
+    url(r"^admin/", include("massadmin.urls")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
+
+if settings.DEBUG and settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
