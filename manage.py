cffd03ec636eeca38937eb740f31ddcf59d28886
Chris Keller <christopherlawrencekeller@gmail.com>
2016-01-26 21:44:50 -0800

    wholesale renaming of the project in order to support applications

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gauging_el_nino.settings_production")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "data_nwsmkr.settings_production")
 
     from django.core.management import execute_from_command_line
 
