fa09ba86d157f4fdfbe33d614153ef4a4acb1038
Chris Keller <christopherlawrencekeller@gmail.com>
2016-02-02 23:13:58 -0800

    moves view to send gauge readings to slack to class-based view

--- la_rain_gauges/urls.py
+++ la_rain_gauges/urls.py
@@ -2,10 +2,10 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.core.urlresolvers import reverse
 from django.views.generic.base import RedirectView
-from . import views
+from .views import NearestRainGauges
 
 app_name = "la_rain_gauges"
 
 urlpatterns = [
-    url(r'rainfall-search/?$', views.main_page, name='main_page'),
+    url(r'rainfall-search/?$', NearestRainGauges.as_view(), name='main_page'),
 ]
--- la_rain_gauges/views.py
+++ la_rain_gauges/views.py
@@ -6,17 +6,18 @@ from django.views.decorators.clickjacking import xframe_options_exempt, xframe_o
 from django.views.generic import View, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.core.urlresolvers import reverse, reverse_lazy
+from django.utils import timezone
 from .models import RainGauge, RainGaugeReading
 from googlegeocoder import GoogleGeocoder
 import calculate
 from haversine import haversine
 # import os
-# import time
-# import datetime
-# from datetime import tzinfo
-# import pytz
-# from pytz import timezone
-# from dateutil import parser
+import time
+import datetime
+from datetime import tzinfo
+import pytz
+from pytz import timezone
+from dateutil import parser
 import logging
 # import yaml
 # import calendar
@@ -24,50 +25,93 @@ import logging
 
 logger = logging.getLogger('data_nwsmkr')
 
-def main_page(request):
-    if request.method=='GET':
-        token = request.GET.get('token', False)
-        if token == 'QHo1Fl7oVvh4QTIJLZkbCet7':
-            user_name = request.GET.get('user_name', '')
-            zip_code = request.GET.get('text', '')
-            list_of_nearby_gauges = []
-            rain_gauges = RainGauge.objects.filter(gauge_type="automatic")
-            geocoder = GoogleGeocoder()
-            try:
-                search = geocoder.get(zip_code)
-                if len(search) > 0:
-                    first_result = search[0]
-                    lat = first_result.geometry.location.lat
-                    lng = first_result.geometry.location.lng
+class NearestRainGauges(ListView):
+
+    queryset = RainGauge.objects.filter(gauge_type="automatic")
+
+    list_of_nearby_gauges = []
+
+    def get(self, request):
+        if request.method=='GET':
+            token = request.GET.get('token', False)
+            # if token == 'QHo1Fl7oVvh4QTIJLZkbCet7':
+            if token == False:
+                user_name = request.GET.get('user_name', '')
+                zip_code = request.GET.get('text', None)
+                task = FunctionalTasks()
+                coordinates = task._get_lat_lng_from(90042)
+                if coordinates:
+                    this_location = (coordinates["lat"], coordinates["lng"])
+                    for gauge in self.queryset:
+                        nearby_gauge = task._assemble_list_of_nearby(this_location, gauge)
+                        if nearby_gauge:
+                            self.list_of_nearby_gauges.append(gauge)
+                    message = self._create_response_message(task, user_name, zip_code)
+                    return StreamingHttpResponse(message)
                 else:
-                    lat = None
-                    lng = None
-            except Exception, exception:
-                logger.error("%s" % (exception))
-            if lat != None and lng != None:
-                this_location = (lat, lng)
-                for gauge in rain_gauges:
-                    comparison_gauge = (gauge.lat_converted, gauge.lng_converted)
-                    evaluated_distance = haversine(this_location, comparison_gauge)
-                    if evaluated_distance < 10:
-                        gauge.distance = float("{0:.2f}".format(evaluated_distance))
-                        list_of_nearby_gauges.append(gauge)
-                if len(list_of_nearby_gauges) == 0:
-                    message = "Hello %s. We couldn't retrieve any data for you about the %s" % (user_name, zip_code)
-                else:
-                    message = "Hello %s. Here are the rain gauges within 10 miles of the %s\n" % (user_name, zip_code)
-                    for item in list_of_nearby_gauges:
-                        message += "\t* %s - %s miles\n" % (item.station_name, item.distance)
-                        gauge = RainGauge.objects.get(id=item.id)
-                        gaugereadings = gauge.raingaugereading_set.order_by("-reading_date_time")
-                        gauge_accumulation = gaugereadings[0].reading_accumulated
-                        gauge_reading = gaugereadings[0].reading_date_time.strftime("%I:%M %p %Z on %a, %b %d, %Y")
-                        message += "\t\t* %s inches accumulated as of %s\n" % (gauge_accumulation, gauge_reading)
+                    return StreamingHttpResponse('The zipcode in your request couldn\'t be processed')
             else:
-                message = "Hello %s. We couldn't retrieve any data for you about the %s" % (user_name, zip_code)
-            return StreamingHttpResponse(message)
+                return StreamingHttpResponse('Your request failed to include the Slack token')
         else:
-            # rain_gauges = RainGauge.objects.filter(gauge_type="automatic")
-            return StreamingHttpResponse('Your request failed to include the Slack token')
-    else:
-        return StreamingHttpResponse('We could not handle your request')
+            return StreamingHttpResponse('We could not process your request')
+
+
+    def _create_response_message(self, task, user_name, zip_code):
+        if len(self.list_of_nearby_gauges) == 0:
+            message = "Hello %s. We couldn't retrieve any data for you about the %s" % (user_name, zip_code)
+        else:
+            message = "Hello %s. Here are the rain gauges within 10 miles of the %s\n" % (user_name, zip_code)
+            for item in self.list_of_nearby_gauges:
+                message += "\t* %s - %s miles\n" % (item.station_name, item.distance)
+                gauge = RainGauge.objects.get(id=item.id)
+                gaugereadings = gauge.raingaugereading_set.order_by("-reading_date_time")
+                gauge_accumulation = gaugereadings[0].reading_accumulated
+                gauge_reading =task._localize_utc_time(gaugereadings[0].reading_date_time)
+                string_gauge_reading = gauge_reading.strftime("%-I:%M %p %Z on %a, %b %-d, %Y")
+                message += "\t\t* %s inches accumulated between Oct. 1 and %s\n" % (gauge_accumulation, string_gauge_reading)
+
+                # gauge.lat_converted, gauge.lng_converted
+
+                message += "\t\t\t* https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCgh93OAbzooidV0OUpIOoc6kTxV5o69do&center=34.167204129001156,-118.32933503906247&zoom=10&size=640x400&scale=2&maptype=roadmap&markers=color:red%7Clabel:%7C34.167204129001156,-118.32933503906247"
+        return message
+
+
+class FunctionalTasks(object):
+
+    local_tz = pytz.timezone ("America/Los_Angeles")
+
+    def _get_lat_lng_from(self, zip_code):
+        """
+        """
+        zip_code = unicode(zip_code)
+        geocoder = GoogleGeocoder()
+        try:
+            search = geocoder.get(zip_code)
+            if len(search) > 0:
+                first_result = search[0]
+                lat = first_result.geometry.location.lat
+                lng = first_result.geometry.location.lng
+                return {"lat": lat, "lng": lng}
+            else:
+                lat = None
+                lng = None
+                return False
+        except Exception, exception:
+            logger.error("%s" % (exception))
+            return False
+
+
+    def _assemble_list_of_nearby(self, this_location, gauge):
+        """
+        """
+        comparison_location = (gauge.lat_converted, gauge.lng_converted)
+        evaluated_distance = haversine(this_location, comparison_location)
+        if evaluated_distance < 10:
+            gauge.distance = float("{0:.2f}".format(evaluated_distance))
+            return gauge
+        else:
+            return False
+
+
+    def _localize_utc_time(self, datetime):
+        return datetime.astimezone(self.local_tz)
