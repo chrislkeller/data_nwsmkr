277f2109754e3c423ee4a1265326c3003409e5bf
Chris Keller <christopherlawrencekeller@gmail.com>
2016-02-02 23:31:51 -0800

    adds static image map to show location of the rain gauge

--- la_rain_gauges/views.py
+++ la_rain_gauges/views.py
@@ -72,8 +72,8 @@ class NearestRainGauges(ListView):
                 gauge_reading =task._localize_utc_time(gaugereadings[0].reading_date_time)
                 string_gauge_reading = gauge_reading.strftime("%-I:%M %p %Z on %a, %b %-d, %Y")
                 message += "\t\t* %s inches accumulated between Oct. 1 and %s\n" % (gauge_accumulation, string_gauge_reading)
-                # image_link = "https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCgh93OAbzooidV0OUpIOoc6kTxV5o69do&center=%s,%s&zoom=15&size=640x400&scale=2&maptype=roadmap&markers=%s,%s\n" % (gauge.lat_converted, gauge.lng_converted, gauge.lat_converted, gauge.lng_converted)
-                # message += image_link
+                image_link = "https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCgh93OAbzooidV0OUpIOoc6kTxV5o69do&center=%s,%s&zoom=15&size=640x400&scale=2&maptype=roadmap&markers=%s,%s\n" % (gauge.lat_converted, gauge.lng_converted, gauge.lat_converted, gauge.lng_converted)
+                message += "\t\t\t* %s" % (image_link)
         return message
 
 
